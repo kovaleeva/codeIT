0131a972d93e22277abf5626f9bc97479f0f4ee4
kovaleeva <kovnatval@gmail.com>
2017-09-25 02:17:24 +0300

    companies smth like complete

--- src/js/companies.js
+++ src/js/companies.js
@@ -55,38 +55,168 @@ $.getJSON('http://codeit.pro/frontTestTask/news/getList',
 
 $.getJSON('http://codeit.pro/frontTestTask/company/getList',
     function (company) {
-        let companylist = company['list'];
-        let companies = $('.list-group');
-        let totalNumber = $('#totalCompanies');
+        let companylist = company['list'],
+            companies = $('#allCompanies'),
+            totalNumber = $('#totalCompanies');
 
         totalNumber.append(companylist.length);
 
         for (var i = 0; i < companylist.length; i++) {
             companies.append('<li class="list-group-item company">' + companylist[i].name + '</li>');
-
         }
         // PARTNENRS
 
         $('.company').on('click', function (e) {
             let partnersBlock = $('.partnersBlock'),
-                partners = $('.partners').empty();
+                partners = $('.partners').empty(),
+                inner = this.innerHTML;
 
             partnersBlock.removeClass('hidden');
             // partnersBlock.toggleClass('active');
             // $(this).toggleClass('active');
 
-            let inner = this.innerHTML;
             for (var i = 0; i < companylist.length; i++) {
-                var comp = companylist[i];
-
+                let comp = companylist[i];
                 if (inner == comp.name) {
-                    for (var j = 0; j < comp.partners.length; i++) {
+                    // let $a = comp['partners'];
+                    //     $a.each(function() {
+                    //         let content = $a.name;
+                    //         let value = $a.value;
+                    //         partners.append('<li><div>' + content + '</div><div class="companyPercent">' + value + '%</div></li>');
+                    //     });
+
+                    for (var j = 0; j < comp.partners.length; j++) {
                         let content = comp.partners[j].name;
                         let value = comp.partners[j].value;
-                        partners.append(content + ' ' + value);
+                        partners.append('<li class="list-group-item justify-content-between">' + content + '<span class="companyPercent badge badge-default badge-pill">' + value + '%</span></li>');
                     }
                 }
             }
+
+            sortPercent();
         });
+
+        // sort by name
+        $('#sortName').on('click', function () {
+            let $elements = $('.partners li'),
+                $target = $('.partners');
+
+            $elements.sort(function (a, b) {
+                let an = $(a).text(),
+                    bn = $(b).text();
+
+                return (an && bn) ? an.toUpperCase().localeCompare(bn.toUpperCase()) : 0;
+            });
+
+            $elements.detach().appendTo($target);
+        });
+
+        // sort percentage from high to small
+        let sortPercent = function () {
+            let item = $('.partners li'),
+                partners = $('.partners').empty();
+            item.each(function () {
+                let i0 = $(this).text().replace(/\D+/g, "");
+                $(this).attr("id", i0);
+            });
+            $(item).sort(function (a, b) {
+                return parseInt(b.id) - parseInt(a.id);
+            }).each(function () {
+                var elem = $(this);
+                elem.remove();
+                $(elem).appendTo(partners);
+            })
+        };
+
+        $('#sortPercent').on('click', function () {
+            sortPercent();
+        });
+
+        // Unique countries
+        let arr = [];
+        for (var i = 0; i < companylist.length; i++) {
+            arr.push(companylist[i].location);
+        }
+
+        let uniqBy = function (a, key) {
+            var seen = {};
+            return a.filter(function (item) {
+                var k = key(item);
+                return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+            })
+        };
+
+        let uniq = uniqBy(arr, JSON.stringify);
+        uniq.forEach(function (item, i, un) {
+            uniq[i].num = 0;
+            arr.forEach(function (it, j, ar) {
+                if (arr[j].name == uniq[i].name) {
+                    uniq[i].num++;
+                }
+            });
+            let legend = $('#companiesHere'),
+                compHere = uniq[i],
+                locationCountry = compHere.name,
+                locationNumber = compHere.num;
+            legend.append('<li class="list-group-item justify-content-between" id="' + locationCountry + '">' + locationCountry + '<span class="companyPercent badge badge-default badge-pill">' + locationNumber + '</span></li>');
+            // console.log(compHere.name);
+
+            // Close/open location
+            $('#' + locationCountry).on('click', function () {
+                $('#location').toggleClass('hidden');
+                $('#showChart').toggleClass('hidden');
+                $('#companiesInCountry').removeClass('hidden');
+
+                getCoutries(this.id);
+                // console.log(this.id);
+            });
+
+            // Get companies in this locationCountry
+            let getCoutries = function (where) {
+                let companiesInCountry = $('#companiesInCountry');
+                for (var i = 0; i < companylist.length; i++) {
+                    if (where == companylist[i].location.name) {
+                        companiesInCountry.append('<li class="list-group-item justify-content-between col-xs-">' + companylist[i].name + '</li>');
+                    }
+                }
+            };
+
+
+            // Close button
+            $('#showChart').on('click', function () {
+                $('#location').removeClass('hidden');
+                $('#showChart').addClass('hidden');
+                $('#companiesInCountry').addClass('hidden').empty();
+
+            });
+
+            // Chart donut
+            google.charts.load("current", {packages: ["corechart"]});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                let array = [
+                    ['Country', 'Number of companies']
+                ];
+                uniq.forEach(function (item, i, un) {
+                    let temp = [];
+                    temp.push(uniq[i].name);
+                    temp.push(uniq[i].num);
+                    array.push(temp);
+                });
+                // console.log(array);
+                // console.log(uniq);
+                var data = google.visualization.arrayToDataTable(array);
+
+                var options = {
+                    pieHole: 0.5,
+                    legend: 'none'
+                    // pieSliceText: 'label'
+                };
+
+                var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+                chart.draw(data, options);
+            };
+        })
 
     });
